f8749f6d1b8b8fab78ccd2b86fc10fc004fafb72
Rudolf Olah <rudolf.olah.to@gmail.com>
2019-12-05 04:29:35 -0500

    added info-box component

---
+++ src/app/info-box/info-box.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'info-box',
+  templateUrl: './info-box.component.html',
+  styleUrls: ['./info-box.component.css']
+})
+export class InfoBoxComponent implements OnInit {
+  @Input() message;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
